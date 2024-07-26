57883d8f70c688ad3afb7ccfff33157c0d8935ca
iTzMvz <matheus.vidal.zenere@gmail.com>
2024-07-26 18:30:49 -0300

    CHORE: Mudanças na estilização do header para web e na tela de login

--- src/components/templates/large/LargeHeader.vue
+++ src/components/templates/large/LargeHeader.vue
@@ -5,25 +5,31 @@ const authStore = useAuthStore();
 </script>
 <template>
   <header>
-    <router-link :to="{ name: 'home' }">Home</router-link> |
-    <router-link to="/categorias">Categorias</router-link> |
-    <router-link to="/acessorios">Acessórios</router-link> |
-    <router-link to="/cores">Cores</router-link> |
-    <router-link to="/marcas">Marcas</router-link> |
-    <router-link to="/modelos">Modelos</router-link> |
-    <router-link to="/veiculos">Veículos</router-link> |
-    <div v-if="authStore.loggedIn">
-    <router-link  to="/logout">Logout</router-link> |
-      {{ authStore.user.email }}
-    </div>
-    <router-link v-else to="/login">Login</router-link>
+    <h1>Garagem</h1>
+    <nav>
+      <router-link :to="{ name: 'home' }">Home</router-link> |
+      <router-link to="/acessorios">Acessórios</router-link> |
+      <router-link to="/categorias">Categorias</router-link> |
+      <router-link to="/cores">Cores</router-link> |
+      <router-link to="/marcas">Marcas</router-link> |
+      <router-link to="/modelos">Modelos</router-link> |
+      <router-link to="/veiculos">Veículos</router-link> |
+      <div v-if="authStore.loggedIn">
+        <router-link  to="/logout">Logout</router-link> |
+        {{ authStore.user.email }}
+      </div>
+      <router-link v-else to="/login">Login</router-link>
+    </nav>
   </header>
 </template>
 <style scoped>
 header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   background-color: #333;
   color: #fff;
-  padding: 10px;
+  padding: 5px;
   text-align: center;
 }
 header a {
@@ -32,5 +38,13 @@ header a {
 }
 header a:hover {
   text-decoration: underline;
+}
+nav {
+  display: flex;
+  gap: 5px;
+  margin-right: 40px;
+}
+h1{
+  margin-left: 20px;
 }
 </style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,10 +3,14 @@ import "@passageidentity/passage-elements/passage-auth";
 </script>
 
 <template>
-  <h1>Login</h1>
+  <h1 class="tittle">Login</h1>
   <div class="authContainer">
     <passage-auth app-id="oRo15hfEZerrD15FuWXRYurQ"></passage-auth>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .tittle {
+    text-align: center;
+  }
+</style>
