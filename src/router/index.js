cbb9b66f79a0996e3e8e66a692e7d178899ab839
iTzMvz <matheus.vidal.zenere@gmail.com>
2024-07-26 09:28:44 -0300

    FEAT: Criação dos layouts para componentização

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import CategoriaView from "../views/CategoriaView.vue";
 import LoginView from "../views/LoginView.vue";
 import LogoutView from "../views/LogoutView.vue";
 
@@ -11,46 +10,48 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
-    },
-    {
-      path: "/categorias",
-      name: "categorias",
-      component: CategoriaView,
-    },
-    {
-      path: "/acessorios",
-      name: "acessorios",
-      component: () => import("../views/AcessorioView.vue"),
-    },
-    {
-      path: "/cores",
-      name: "cores",
-      component: () => import("../views/CorView.vue"),
-    },
-    {
-      path: "/marcas",
-      name: "marcas",
-      component: () => import("../views/MarcaView.vue"),
-    },
-    {
-      path: "/modelos",
-      name: "modelos",
-      component: () => import("../views/ModeloView.vue"),
-    },
-    {
-      path:"/veiculos",
-      name:"veiculos",
-      component: () => import("../views/VeiculoView.vue"),
-    },
-    {
-      path: "/login",
-      name: "login",
-      component: LoginView,
-    },
-    {
-      path: "/logout",
-      name: "logout",
-      component: LogoutView,
+      children: [
+        {
+          path: "/categorias",
+          name: "categorias",
+          component: () => import("../views/CategoriaView.vue"),
+        },
+        {
+          path: "/acessorios",
+          name: "acessorios",
+          component: () => import("../views/AcessorioView.vue"),
+        },
+        {
+          path: "/cores",
+          name: "cores",
+          component: () => import("../views/CorView.vue"),
+        },
+        {
+          path: "/marcas",
+          name: "marcas",
+          component: () => import("../views/MarcaView.vue"),
+        },
+        {
+          path: "/modelos",
+          name: "modelos",
+          component: () => import("../views/ModeloView.vue"),
+        },
+        {
+          path:"/veiculos",
+          name:"veiculos",
+          component: () => import("../views/VeiculoView.vue"),
+        },
+        {
+          path: "/login",
+          name: "login",
+          component: LoginView,
+        },
+        {
+          path: "/logout",
+          name: "logout",
+          component: LogoutView,
+        }
+      ],
     }
   ],
 });
