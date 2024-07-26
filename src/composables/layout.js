479f6d302718f6953129973f90b175c2297b22f8
iTzMvz <matheus.vidal.zenere@gmail.com>
2024-07-26 18:29:14 -0300

    REFACTOR: exclusão do layout de tamanho médio

--- src/composables/layout.js
+++ src/composables/layout.js
@@ -7,8 +7,6 @@ export function useLayout() {
     const width = window.innerWidth
     if (width < 768) {
       layout.value = defineAsyncComponent(() => import('@/layouts/LayoutSmall.vue'))
-    } else if (width < 1200) {
-      layout.value = defineAsyncComponent(() => import('@/layouts/LayoutMedium.vue'))
     } else {
       layout.value = defineAsyncComponent(() => import('@/layouts/LayoutLarge.vue'))
     }
